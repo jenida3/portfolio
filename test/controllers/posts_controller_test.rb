2e90840a39fa335f6b1b344beaefd40b9aa31e16
jenida3 <m9w-o-w@ezweb.ne.jp>
2024-11-09 20:10:53 +0900

    AAA

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -2,6 +2,7 @@ require "test_helper"
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
+    skip "このテストはまだ準備中です"
     get posts_index_url
     assert_response :success
   end
