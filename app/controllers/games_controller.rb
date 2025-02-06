7fe611f887e28a940d93ffb244df26c555c0ae3e
jenida3 <m9w-o-w@ezweb.ne.jp>
2025-02-06 19:20:19 +0900

    ゴリむずい

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,12 +1,7 @@
 class GamesController < ApplicationController
   def show
     @game = Game.find(params[:id])
-    @post = Post.new(game: @game)
-    @posts = @game.posts.order(created_at: :desc)
-
-    # ゲーム掲示板ページのタイトルと説明を設定
-    @page_title = @game.title
-    @page_description = "#{@game.title}の掲示板 - TactiZone"
+    @post = Post.new(game: @game) # 新しい投稿用のインスタンスを作成
+    @posts = @game.posts.order(created_at: :desc) # 投稿を作成日時の降順で取得
   end
 end
-
