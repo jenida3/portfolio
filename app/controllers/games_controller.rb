07d43ed5a2232b9f63616aa9258fc270997f4dd9
jenida3 <m9w-o-w@ezweb.ne.jp>
2025-02-06 18:19:52 +0900

    動的に変更

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,11 @@
 class GamesController < ApplicationController
   def show
     @game = Game.find(params[:id])
-    @post = Post.new(game: @game) # 新しい投稿用のインスタンスを作成
-    @posts = @game.posts.order(created_at: :desc) # 投稿を作成日時の降順で取得
+    @post = Post.new(game: @game)  # 新しい投稿用のインスタンスを作成
+    @posts = @game.posts.order(created_at: :desc)  # 投稿を作成日時の降順で取得
+
+    # OGPやTwitterカード用のタイトルと説明を設定
+    content_for :title, @game.title
+    content_for :description, @game.description || "掲示板: #{@game.title} - TactiZone"
   end
 end
