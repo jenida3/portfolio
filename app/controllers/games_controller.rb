8bc92332358e58b0d34df923b2a871f71af5c0f8
jenida3 <m9w-o-w@ezweb.ne.jp>
2025-02-06 18:33:34 +0900

    動的に変更

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,12 +1,12 @@
+# app/controllers/games_controller.rb
 class GamesController < ApplicationController
   def show
     @game = Game.find(params[:id])
     @post = Post.new(game: @game)
     @posts = @game.posts.order(created_at: :desc)
-    
-    # ページ固有のタイトルと説明をビューに渡す
-    content_for :title, @game.title
-    content_for :description, @game.description || "掲示板: #{@game.title} - TactiZone"
+
+    # ページ固有のタイトルと説明をインスタンス変数に設定
+    @page_title = @game.title
+    @page_description = @game.description || "掲示板: #{@game.title} - TactiZone"
   end
 end
-
